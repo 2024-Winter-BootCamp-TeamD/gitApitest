a2d42aa9a72d999146ee7feb6ded7defb3b4e5cc
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-01-02 16:42:39 +0900

    웹훅 걸기까지 테스트용 콛드

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from mysite.views import github_oauth, github_oauth_callback, org_list, org_repos, setup_webhook, github_webhook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # GitHub OAuth 인증
+    path('github/oauth/', github_oauth, name='github_oauth'),
+    path('github/oauth/callback/',github_oauth_callback, name='github_oauth_callback'),
+
+    # 조직 목록 및 리포지토리 목록 페이지
+    path('github/orgs/', org_list, name='org_list'),
+    path('github/orgs/<str:org_name>/repos/', org_repos, name='org_repos'),
+
+    # 리포지토리에 웹훅 설정
+    path('github/orgs/<str:org_name>/repos/<str:repo_name>/setup-webhook/', setup_webhook, name='setup_webhook'),
+
+    # GitHub에서 웹훅 이벤트를 수신하는 엔드포인트
+    path('github/webhook/',github_webhook, name='github_webhook'),
 ]
